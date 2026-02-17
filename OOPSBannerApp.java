821c8bede92763db56baf3bc97581b5a7cae51b4
rudrasingh14v-byte <rudra.singhv14@gmail.com>
2026-02-17 11:22:50 +0530

    Used String.join() method to print banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,18 @@
  * This class demonstrates a simple Java application that displays the object
  * Oriented Programming System oops acronym to the console.
  * @author Rudra Singh
- * @version 1.0
+ * @version 1.1
  */
 
 public class OOPSBannerApp {
     public static void main(String args[]) {
-        System.out.println("   ***.     "+" "+"   ***.     "+" "+"******   "+" "+"   *****    ");
-        System.out.println(" **    **   "+" "+" **    **   "+" "+"**    ** "+" "+" **    **   ");
-        System.out.println("**      **  "+" "+"**      **  "+" "+"**    ** "+" "+"**          ");
-        System.out.println("**      **  "+" "+"**      **  "+" "+"**   **  "+" "+"  ******     ");
-        System.out.println("**      **  "+" "+"**      **  "+" "+"** **    "+" "+"        **  ");
-        System.out.println(" **    **   "+" "+" **    **   "+" "+"**       "+" "+" **     ** ");
-        System.out.println("   ***      "+" "+"   ***      "+" "+"**       "+" "+"   *****   ");
+        System.out.println(String.join(" ", "   ***     ", "   ***.    ", "******   ", "   *****    "));
+        System.out.println(String.join(" ", " **    **  ", " **    **  ", "**    ** ", " **    **   "));
+        System.out.println(String.join(" ", "**      ** ", "**      ** ", "**    ** ", "**          "));
+        System.out.println(String.join(" ", "**      ** ", "**      ** ", "**   **  ", " ******     "));
+        System.out.println(String.join(" ", "**      ** ", "**      ** ", "** **    ", "       **   "));
+        System.out.println(String.join(" ", " **    **  ", " **    **  ", "**       ", " **     **  "));
+        System.out.println(String.join(" ", "   ***     ", "   ***     ", "**       ", "   *****    "));
 
         
     }
