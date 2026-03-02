a922f5ee0ac75302537c0e007c8ebd4e1055773c
rudrasingh14v-byte <rudra.singhv14@gmail.com>
2026-03-02 08:24:48 +0530

    Used array init for concise code

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,20 +2,20 @@
  * This class demonstrates a simple Java application that displays the object
  * Oriented Programming System oops acronym to the console.
  * @author Rudra Singh
- * @version 1.1
+ * @version 1.3
  */
 
 public class OOPSBannerApp {
     public static void main(String args[]) {
-        String[] lines = new String[7];
-        lines[0]=String.join(" ", "   ***     ", "   ***.    ", "******   ", "   *****    ");
-        lines[1]=String.join(" ", " **    **  ", " **    **  ", "**    ** ", " **    **   ");
-        lines[2]=String.join(" ", "**      ** ", "**      ** ", "**    ** ", "**          ");
-        lines[3]=String.join(" ", "**      ** ", "**      ** ", "**   **  ", " ******     ");
-        lines[4]=String.join(" ", "**      ** ", "**      ** ", "** **    ", "       **   ");
-        lines[5]=String.join(" ", " **    **  ", " **    **  ", "**       ", " **     **  ");
-        lines[6]=String.join(" ", "   ***     ", "   ***     ", "**       ", "   *****    ");
-
+        String[] lines = {
+        String.join(" ", "   ***     ", "   ***.    ", "******   ", "   *****    "),
+        String.join(" ", " **    **  ", " **    **  ", "**    ** ", " **    **   "),
+        String.join(" ", "**      ** ", "**      ** ", "**    ** ", "**          "),
+        String.join(" ", "**      ** ", "**      ** ", "**   **  ", " ******     "),
+        String.join(" ", "**      ** ", "**      ** ", "** **    ", "       **   "),
+        String.join(" ", " **    **  ", " **    **  ", "**       ", " **     **  "),
+        String.join(" ", "   ***     ", "   ***     ", "**       ", "   *****    "),
+        };
     for(String print : lines)
         {
             System.out.println(print);
