df65de061fbf5a3a4adec9c854754c2bfd7c8254
rudrasingh14v-byte <rudra.singhv14@gmail.com>
2026-03-09 23:15:16 +0530

    Changed the code to use methods for versatility

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,23 +2,58 @@
  * This class demonstrates a simple Java application that displays the object
  * Oriented Programming System oops acronym to the console.
  * @author Rudra Singh
- * @version 1.3
- */
+ * @version 1.5
+ **/
 
 public class OOPSBannerApp {
-    public static void main(String args[]) {
-        String[] lines = {
-        String.join(" ", "   ***     ", "   ***.    ", "******   ", "   *****    "),
-        String.join(" ", " **    **  ", " **    **  ", "**    ** ", " **    **   "),
-        String.join(" ", "**      ** ", "**      ** ", "**    ** ", "**          "),
-        String.join(" ", "**      ** ", "**      ** ", "**   **  ", " ******     "),
-        String.join(" ", "**      ** ", "**      ** ", "** **    ", "       **   "),
-        String.join(" ", " **    **  ", " **    **  ", "**       ", " **     **  "),
-        String.join(" ", "   ***     ", "   ***     ", "**       ", "   *****    "),
+    public static String[] getO() {
+        String a[] = {
+            "  ***  ",
+            " ** ** ",
+            "**   **",
+            "**   **",
+            "**   **",
+            " ** ** ",
+            "  ***  "
         };
-    for(String print : lines)
-        {
-            System.out.println(print);
-        }        
+        return a;
+    }
+    
+    public static String[] getP() {
+        String b[] = {
+            "*****  ",
+            "**   **",
+            "**   **",
+            "*****  ",
+            "**     ",
+            "**     ",
+            "**     "
+        };
+        return b;
+    }
+    
+    public static String[] getS() {
+        String c[] = {
+            " ***** ",
+            "**     ",
+            "**     ",
+            " ****  ",
+            "     **",
+            "**   **",
+            " ****  "
+        };
+        return c;
+    }
+    
+    public static void main(String args[]) {
+        String[][] OOPSPattern = {getO(), getO(), getP(), getS()};
+        
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(OOPSPattern[j][i] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
